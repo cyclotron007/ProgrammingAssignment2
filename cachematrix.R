5b7bec0a20568f8e3189db620b977c31717299dd
Arvind Kumar <arvindkumar.dce@gmail.com>
2014-12-17 15:35:07 +0530

    Programming Assignment 2: Lexical Scoping V1
    This is my first version of PA2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script contains two functions that are used to create a special matrix
+## object that stores a matrix and cache's its inverse.
 
-## Write a short comment describing this function
+
+
+## The makeCacheMatrix function takes a matrix as its input and creates 
+## a special matrix object which have 4 functions for getting and setting matrix 
+## and its inverse and 1 data variable to store its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  solution <- NULL
+    
+  getmatrix <- function(){x}
+  setsolution <- function(solve) { solution <<- solve}
+  
+  getsolution <- function() {solution}
+  setmatrix <- function(y = matrix()) {
+    x <<- y
+    solution <<- NULL
+  }
+  
+  list(setmatrix = setmatrix,
+       getmatrix = getmatrix,
+       setsolution = setsolution,
+       getsolution = getsolution)
+  
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function computes the inverse of the special "matrix" returned
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then cacheSolve should retrieve the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ 
+  solve <- x$getsolution()
+  
+  if(!is.null(solve)){
+    message("getting cached data")
+    return(solve)
+  }
+  
+  tosolve <- x$getmatrix()
+  solve <- solve(tosolve)
+  x$setsolution(solve)
+  solve
+  
 }
